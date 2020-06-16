9c8540a8b0df6a1b6a349480e445a050e700387d
Ubuntu <ubuntu@ip-172-31-18-4.us-east-2.compute.internal>
2020-06-16 16:54:58 +0000

    protobuffers

--- cmd/dev.setup.sh
+++ cmd/dev.setup.sh
@@ -5,10 +5,60 @@
 #     return 1
 # fi
 
+if !(unzip --version); then
+    sudo apt -y install unzip
+fi
+
+if !(autoconf --version); then
+    sudo apt -y install autoconf
+fi
+
+if !(automake --version); then
+    sudo apt -y install automake
+fi
+
+if !(libtool --version); then
+    sudo apt -y install libtool
+fi
+
+if !(curl --version); then
+    sudo apt -y install curl
+fi
+
+if !(make --version); then
+    sudo apt -y install make
+fi
+
+if !(tree --version); then
+    sudo apt install tree
+fi
+
+if !(g++ --version); then
+    sudo apt install g++
+fi
+
+if !(figlet -v); then
+    sudo apt install figlet
+fi
+
 if !(docker --version); then
     sudo apt install -y docker.io
     sudo groupadd docker
     sudo gpasswd -a $USER docker
+fi
+
+if !(protoc --version); then
+    curl -LO https://github.com/protocolbuffers/protobuf/archive/v3.12.3.zip
+    
+    unzip v3.12.3.zip
+    rm -rf v3.12.3.zip
+
+    cd ./protobuf-3.12.3
+    ./autogen.sh && ./configure
+    make && make check && make install
+    
+    cd ..
+    sudo ldconfig # refresh shared librery cache.
 fi
 
 if !(go version); then
@@ -21,18 +71,14 @@ if !(go version); then
         return 1
     fi
 
-    rm -rf go1.14.4.linux-amd64.tar.gz
     printf "\n\n# Added automatically by a setup script at $(date)" $USER >> ~/.bashrc
     printf "\nexport PATH=\$PATH:/usr/local/go/bin\n" $USER >> ~/.bashrc
+
+    rm -rf go1.14.4.linux-amd64.tar.gz
 fi
 
-if !(tree --version); then
-    sudo apt install tree
-fi
-
-if !(figlet -v); then
-    sudo apt install figlet
-fi
+sudo apt update
+sudo apt upgrade
 
 figlet "This is your ubuntu :)"
 echo "Done."
